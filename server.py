7f5a3850657fd59be7c43f15055cd18d2b4db06d
Cristian Hernández <chtello05@gmail.com>
2023-08-30 20:24:45 -0600

    api terminada

--- server.py
+++ server.py
@@ -1,90 +1,132 @@
-import os
-from flask import Flask, send_from_directory, render_template, redirect,jsonify
+# import os
+# from flask import Flask, send_from_directory, render_template, redirect,jsonify
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_marshmallow import Marshmallow
+# from flask_cors import CORS
+# from sqlalchemy import text
+# app = Flask(__name__)
+# CORS(app)
+# port = int(os.environ.get("PORT", 5000))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://uwcz1z8tw0nrh0ey:OeX1Pjhhuve1VA5v0Pd8@bmotoxcjvbuzkz2ic55p-mysql.services.clever-cloud.com:3306/bmotoxcjvbuzkz2ic55p'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+# db=SQLAlchemy(app)
+# ma=Marshmallow(app)
+
+
+# @app.route('/',methods=['GET'])
+# def home():
+#     print("API CONECTADA")
+#     query = text("SHOW TABLES;")
+#     conn = db.engine.connect()
+#     result = conn.execute(query)
+#     column_names = result.keys()
+#     # Construir una lista de diccionarios con los resultados
+#     results_list = [dict(zip(column_names, row)) for row in result]
+#     print("RESPONSE",results_list)
+#     # Cerrar la conexión
+#     conn.close()
+
+#     # Retornar los resultados como JSON en la respuesta
+#     return jsonify(results_list)
+#     # Retornar los resultados como JSON en la respuesta
+#     #return jsonify("Api Conectada")
+
+# @app.route('/aire',methods=['GET'])
+# def aire():
+#     # Obtener una conexión a la base de datos
+#     conn = db.engine.connect()
+
+#     # Realizar la consulta SELECT *
+#     query = text("SELECT * FROM Aire")
+#     result = conn.execute(query)
+#     column_names = result.keys()
+
+#     # Construir una lista de diccionarios con los resultados
+#     results_list = [dict(zip(column_names, row)) for row in result]
+#     # Cerrar la conexión
+#     conn.close()
+
+#     # Retornar los resultados como JSON en la respuesta
+#     return jsonify(results_list)
+
+# @app.route('/ilumina',methods=['GET'])
+# def ilumina():
+#     # Obtener una conexión a la base de datos
+#     conn = db.engine.connect()
+
+#     # Realizar la consulta SELECT *
+#     query = text("SELECT * FROM Iluminacion")
+#     result = conn.execute(query)
+#     column_names = result.keys()
+
+#     # Construir una lista de diccionarios con los resultados
+#     results_list = [dict(zip(column_names, row)) for row in result]
+#     # Cerrar la conexión
+#     conn.close()
+
+#     # Retornar los resultados como JSON en la respuesta
+#     return jsonify(results_list)
+
+# @app.route('/tempe',methods=['GET'])
+# def tempe():
+#     # Obtener una conexión a la base de datos
+#     conn = db.engine.connect()
+
+#     # Realizar la consulta SELECT *
+#     query = text("SELECT * FROM Temperatura")
+#     result = conn.execute(query)
+#     column_names = result.keys()
+
+#     # Construir una lista de diccionarios con los resultados
+#     results_list = [dict(zip(column_names, row)) for row in result]
+#     # Cerrar la conexión
+#     conn.close()
+
+#     # Retornar los resultados como JSON en la respuesta
+#     return jsonify(results_list)
+
+# @app.route('/actual',methods=['GET'])
+# def actual():
+#     # Obtener una conexión a la base de datos
+#     conn = db.engine.connect()
+
+#     # Realizar la consulta SELECT *
+#     query = text("SELECT * FROM actual order by id desc limit 1")
+#     result = conn.execute(query)
+#     column_names = result.keys()
+#     # Construir una lista de diccionarios con los resultados
+#     results_list = [dict(zip(column_names, row)) for row in result]
+#     # Cerrar la conexión
+#     conn.close()
+
+#     # Retornar los resultados como JSON en la respuesta
+#     return jsonify(results_list[0])
+
+# if __name__ == "__main__":
+#     app.run(port=port)
+
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from sqlalchemy import text
-app = Flask(__name__)
+import mysql.connector
+app=Flask(__name__)
 CORS(app)
-port = int(os.environ.get("PORT", 5000))
+#app.config['SQL_ALCHEMY_DATABASE_URI']='sqlite:///database.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://uwcz1z8tw0nrh0ey:OeX1Pjhhuve1VA5v0Pd8@bmotoxcjvbuzkz2ic55p-mysql.services.clever-cloud.com:3306/bmotoxcjvbuzkz2ic55p'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db=SQLAlchemy(app)
 ma=Marshmallow(app)
 
-
 @app.route('/',methods=['GET'])
 def home():
     print("API CONECTADA")
-    query = text("SHOW TABLES;")
-    conn = db.engine.connect()
-    result = conn.execute(query)
-    column_names = result.keys()
-    # Construir una lista de diccionarios con los resultados
-    results_list = [dict(zip(column_names, row)) for row in result]
-    print("RESPONSE",results_list)
-    # Cerrar la conexión
-    conn.close()
 
     # Retornar los resultados como JSON en la respuesta
-    return jsonify(results_list)
-    # Retornar los resultados como JSON en la respuesta
-    #return jsonify("Api Conectada")
-
-@app.route('/aire',methods=['GET'])
-def aire():
-    # Obtener una conexión a la base de datos
-    conn = db.engine.connect()
-
-    # Realizar la consulta SELECT *
-    query = text("SELECT * FROM Aire")
-    result = conn.execute(query)
-    column_names = result.keys()
-
-    # Construir una lista de diccionarios con los resultados
-    results_list = [dict(zip(column_names, row)) for row in result]
-    # Cerrar la conexión
-    conn.close()
-
-    # Retornar los resultados como JSON en la respuesta
-    return jsonify(results_list)
-
-@app.route('/ilumina',methods=['GET'])
-def ilumina():
-    # Obtener una conexión a la base de datos
-    conn = db.engine.connect()
-
-    # Realizar la consulta SELECT *
-    query = text("SELECT * FROM Iluminacion")
-    result = conn.execute(query)
-    column_names = result.keys()
-
-    # Construir una lista de diccionarios con los resultados
-    results_list = [dict(zip(column_names, row)) for row in result]
-    # Cerrar la conexión
-    conn.close()
-
-    # Retornar los resultados como JSON en la respuesta
-    return jsonify(results_list)
-
-@app.route('/tempe',methods=['GET'])
-def tempe():
-    # Obtener una conexión a la base de datos
-    conn = db.engine.connect()
-
-    # Realizar la consulta SELECT *
-    query = text("SELECT * FROM Temperatura")
-    result = conn.execute(query)
-    column_names = result.keys()
-
-    # Construir una lista de diccionarios con los resultados
-    results_list = [dict(zip(column_names, row)) for row in result]
-    # Cerrar la conexión
-    conn.close()
-
-    # Retornar los resultados como JSON en la respuesta
-    return jsonify(results_list)
+    return jsonify("Api Conectada")
 
 @app.route('/actual',methods=['GET'])
 def actual():
@@ -92,16 +134,191 @@ def actual():
     conn = db.engine.connect()
 
     # Realizar la consulta SELECT *
-    query = text("SELECT * FROM actual order by id desc limit 1")
+    query = text("SELECT * FROM actual ORDER BY id DESC LIMIT 1")
     result = conn.execute(query)
     column_names = result.keys()
+
     # Construir una lista de diccionarios con los resultados
     results_list = [dict(zip(column_names, row)) for row in result]
+    print(results_list)
     # Cerrar la conexión
     conn.close()
 
     # Retornar los resultados como JSON en la respuesta
-    return jsonify(results_list[0])
+    return jsonify(results_list)
 
-if __name__ == "__main__":
-    app.run(port=port)
+connection = mysql.connector.connect(
+    user='uwcz1z8tw0nrh0ey',
+    password='OeX1Pjhhuve1VA5v0Pd8',
+    host='bmotoxcjvbuzkz2ic55p-mysql.services.clever-cloud.com',
+    database='bmotoxcjvbuzkz2ic55p',
+    port='3306'
+)
+
+@app.route('/encender_vent1',methods=['GET'])
+def encender_vent1():
+    cursor = connection.cursor()  
+    sql = "INSERT INTO Temperatura (Ventilacion) VALUES (1)"
+    cursor.execute(sql) 
+    connection.commit() 
+    # Cerrar la conexión
+    cursor.close()
+
+    # Retornar los resultados como JSON en la respuesta
+    return jsonify()
+
+@app.route('/encender_vent2',methods=['GET'])
+def encender_vent2():
+    cursor = connection.cursor()  
+    sql = "INSERT INTO Temperatura (Ventilacion) VALUES (2)"
+    cursor.execute(sql) 
+    connection.commit() 
+    # Cerrar la conexión
+    cursor.close()
+
+    # Retornar los resultados como JSON en la respuesta
+    return jsonify()
+
+@app.route('/apagar_vent',methods=['GET'])
+def apagar_vent():
+    cursor = connection.cursor()  
+    sql = "INSERT INTO Temperatura (Ventilacion) VALUES (0)"
+    cursor.execute(sql) 
+    connection.commit() 
+    # Cerrar la conexión
+    cursor.close()
+
+    # Retornar los resultados como JSON en la respuesta
+    return jsonify()
+
+@app.route('/encender_luz',methods=['GET'])
+def encender_luz():
+    # Obtener una conexión a la base de datos
+    conn = db.engine.connect()
+
+    # Realizar la consulta SELECT *
+    query = text("SELECT * FROM actual ORDER BY actual.id DESC LIMIT 1")
+    result = conn.execute(query)
+    column_names = result.keys()
+
+    # Construir una lista de diccionarios con los resultados
+    results_list = [dict(zip(column_names, row)) for row in result]
+    print(results_list)
+    # Cerrar la conexión
+    conn.close()
+
+    Presencia_Humana_value = results_list[0]['Presencia_Humana']
+    #se puede realizar operaciones
+    if Presencia_Humana_value == 1 :
+        cursor = connection.cursor()  
+        sql = "INSERT INTO Iluminacion (Iluminacion_habitacion,Iluminacion_activa_1,Iluminacion_activa_2) VALUES (%s,%s,%s)"
+        cursor.execute(sql,(1,0,0)) 
+        connection.commit() 
+        # Cerrar la conexión
+        conn.close()
+        cursor.close()
+
+    # Retornar los resultados como JSON en la respuesta
+    return jsonify(results_list)
+
+@app.route('/apagar_luz',methods=['GET'])
+def apagar_luz():
+    # Obtener una conexión a la base de datos
+    conn = db.engine.connect()
+
+    # Realizar la consulta SELECT *
+    query = text("SELECT * FROM actual ORDER BY id DESC LIMIT 1")
+    result = conn.execute(query)
+    column_names = result.keys()
+
+    # Construir una lista de diccionarios con los resultados
+    results_list = [dict(zip(column_names, row)) for row in result]
+    print(results_list)
+    # Cerrar la conexión
+    conn.close()
+
+    Presencia_Humana_value = results_list[0]['Presencia_Humana']
+    #se puede realizar operaciones
+    if Presencia_Humana_value == 1:
+        cursor = connection.cursor()  
+        sql = "INSERT INTO Iluminacion (Iluminacion_habitacion,Iluminacion_activa_1,Iluminacion_activa_2) VALUES (%s,%s,%s)"
+        cursor.execute(sql,(0,0,0)) 
+        connection.commit() 
+        # Cerrar la conexión
+        conn.close()
+        cursor.close()
+
+    # Retornar los resultados como JSON en la respuesta
+    return jsonify(results_list)
+
+
+@app.route('/notificacion',methods=['GET'])
+def notificacion():    
+    data={
+        "aire1":0,
+        "aire2":0,
+        "luz1":0,
+        "luz2":0
+    }
+    # Obtener una conexión a la base de datos
+    conn = db.engine.connect()
+
+    # Realizar la consulta notificaicon aire
+    query = text("SELECT * FROM Aire ORDER BY id DESC LIMIT 1")
+    result = conn.execute(query)
+    column_names = result.keys()
+    # Construir una lista de diccionarios con los resultados
+    results_list = [dict(zip(column_names, row)) for row in result]
+    aire_habitacion_1_value = results_list[0]['aire_habitacion_1']
+    aire_habitacion_2_value = results_list[0]['aire_habitacion_2']
+    
+    # Realizar la consulta notificaicon aire
+    query = text("SELECT * FROM iluminacion ORDER BY id DESC LIMIT 1")
+    result = conn.execute(query)
+    column_names = result.keys()
+    # Construir una lista de diccionarios con los resultados
+    results_list = [dict(zip(column_names, row)) for row in result]
+    print(results_list)
+    iluminacion_habitacion_value = results_list[0]['Iluminacion_habitacion']
+    iluminacion_activa_1_value = results_list[0]['Iluminacion_activa_1']
+    iluminacion_activa_2_value = results_list[0]['Iluminacion_activa_2']
+
+    #validaciones aire
+    if  aire_habitacion_1_value == 1 or aire_habitacion_2_value ==1:
+        cursor = connection.cursor()  
+        sql = "INSERT INTO aire (aire_habitacion_1,aire_habitacion_2) VALUES (%s,%s)"
+        cursor.execute(sql,(0,0)) 
+        connection.commit() 
+        # Cerrar la conexión
+        conn.close()
+        cursor.close()
+        if aire_habitacion_1_value == 1: 
+            data["aire1"]=1
+        else:
+            data["aire2"]=1
+    #validaciones iluminacion
+    if  iluminacion_activa_1_value == 1 or iluminacion_activa_2_value ==1:
+        cursor = connection.cursor()  
+        sql = "INSERT INTO Iluminacion (Iluminacion_habitacion,Iluminacion_activa_1,Iluminacion_activa_2) VALUES (%s,%s,%s)"
+        cursor.execute(sql,(iluminacion_habitacion_value,0,0)) 
+        connection.commit() 
+        # Cerrar la conexión
+        conn.close()
+        cursor.close()
+        if iluminacion_activa_1_value == 1: 
+            data["luz1"]=1
+        else:
+            data["luz2"]=1
+    
+
+    # Cerrar la conexión
+    conn.close()
+    
+    #print(data)
+    # Retornar los resultados como JSON en la respuesta
+    return jsonify(data)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True,host="0.0.0.0")
